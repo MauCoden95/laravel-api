89dc0cab92560ef01e790621dc5ca3831b5f07d8
Tu Nombre <you@example.com>
2024-07-19 13:27:21 -0300

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the products.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+}
